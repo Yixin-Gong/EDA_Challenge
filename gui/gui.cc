167e7b00824f804a2ba817e101153acd1c2d1e2e
Lao-Zhu <2208213223@qq.com>
2022-09-23 23:20:44 +0800

    fix(gui): Fix the problem of wrong slot function in gui.
    [skip ci]

--- gui/gui.cc
+++ gui/gui.cc
@@ -1,6 +1,10 @@
-//
-// Created by ubuntu on 22-9-23.
-//
+/**************************************************************************//**
+  \file     gui.cc
+  \brief    This document describes the interface of the main form.
+  \author   Lao·Zhu
+  \version  V1.0.1
+  \date     23. September 2022
+ ******************************************************************************/
 
 #include "gui.h"
 #include <gtk/gtk.h>
@@ -9,7 +13,7 @@ static void gui_button_callback(GtkWidget *widget, gpointer data) {
     g_print("Hello World\n");
 }
 
-static void gui_app_callback(GtkApplication *app, gpointer user_data) {
+static void gui_app_active(GtkApplication *app, gpointer user_data) {
     GtkWidget *window;
     GtkWidget *button;
 
@@ -27,8 +31,8 @@ static void gui_app_callback(GtkApplication *app, gpointer user_data) {
 int gui_display_main_window(int argc, char **argv) {
     GtkApplication *app;
     app = gtk_application_new("eda.statistic.tools", G_APPLICATION_FLAGS_NONE);
-    g_signal_connect (app, "gui_app_callback", G_CALLBACK(gui_app_callback), NULL);
-    int status = g_application_run(G_APPLICATION (app), 1, argv);
+    g_signal_connect (app, "activate", G_CALLBACK(gui_app_active), NULL);
+    int status = g_application_run(G_APPLICATION (app), argc, argv);
     g_object_unref(app);
     return status;
 }
--- gui/gui.h
+++ gui/gui.h
@@ -1,6 +1,10 @@
-//
-// Created by ubuntu on 22-9-23.
-//
+/**************************************************************************//**
+  \file     gui.h
+  \brief    This header file describes the functions of the main form.
+  \author   Lao·Zhu
+  \version  V1.0.1
+  \date     23. September 2022
+ ******************************************************************************/
 
 #ifndef EDA_CHALLENGE_GUI_GUI_H_
 #define EDA_CHALLENGE_GUI_GUI_H_
