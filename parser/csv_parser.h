c451bbdc9ede6ab54b02068220ffbe2b2738b1e6
zzj <1511932471@qq.com>
2022-11-21 20:10:59 +0800

    docs(parser): Added a comment to the function.

--- parser/csv_parser.h
+++ parser/csv_parser.h
@@ -17,26 +17,43 @@
 #include <list>
 #include "hopscotch_map.h"
 
+/*!
+  \struct CSVSignalStatisticStruct
+  \brief  Structs for signal statistics
+ */
 struct CSVSignalStatisticStruct {
-  int tc, t1, t0, tx, tg;
+  int tc, t1, t0, tx, tg;          ///< The information cut from the csv file
   double sp;
 };
 
+/*!
+  \struct CSV_VCDSignalStruct
+  \brief  Structs for signal properties
+ */
 struct CSV_VCDSignalStruct {
-  unsigned int vcd_signal_width;
-  std::string vcd_signal_label;
-  int declare_width_start = 0;
+  unsigned int vcd_signal_width;   ///< Signal bit width
+  std::string vcd_signal_label;    ///< Label of the signal
+  int declare_width_start = 0;     ///< Initial bits of the vector signal
 };
 
+/*!  \brief CSV parser class, which contains all the CSV file parsing functions. */
 class CSVParser {
  public:
+
+  /*!  \brief Constructor for the CSV parser class, which opens the CSV file. */
   explicit CSVParser(const std::string &filename) {
       fp_ = fopen64(filename.c_str(), "r");
       std::cout << "\nOpen file: " << filename << "\n";
   }
+
+  /*!  \brief The destructor function of the CSV parser class, which clears all the hash tables to free memory. */
   ~CSVParser() {
       fclose(fp_);
+      csv_signal_table_.clear();
+      csv_vcd_signal_table_.clear();
+      merge_csv_vcd_table_.clear();
   }
+
   void parse_csv();
   void get_vcd_scope(const std::string &vcd_filename);
   void csv_find_vcd();
